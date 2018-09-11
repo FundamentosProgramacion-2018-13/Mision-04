565b9234757dd40193cc9386bfac9ff529580caf
lKlausl <42352993+lKlausl@users.noreply.github.com>
2018-09-10 22:05:40 -0500

    Create Triangulos.py
    Claudio Mayoral García
    A01747749
    Grupo 04 LuJu

---
+++ Triangulos.py
@@ -0,0 +1,31 @@
+#Autor Claudio Mayoral García
+#Descripción Dice que tipo de triángulo es dependiendo de los valores que se les asignen a los lados
+#Y si no cumple con las características del triángulo manda un mensaje de error
+
+
+#Esta función regresa el tipo de triángulo que es pero si no es un triángulo regresa un anuncio de error
+def esTriangulo(numero, numero2, numero3):
+    if numero <= 0 or numero2 <= 0 or numero3 <= 0:
+        return "El triángulo no existe"
+    if (numero + numero2) <= numero3 or (numero + numero3) <= numero2 or (numero3 + numero2) <= numero:
+        return "El triángulo no existe"
+    elif numero**2 + numero2**2 == numero3**2 or numero**2 + numero3**2 == numero2**2 or numero2**2 + numero3**2 == numero**2:
+        return "Es un triángulo rectangulo"
+    elif numero == numero2 == numero3:
+        return "Es un triángulo equilatero"
+    elif numero == numero2 or numero ==numero3 or numero2 == numero3:
+        return "Es un triángulo isóceles"
+
+
+#Función principal, pide valores de los lados del triángulo y manda a imprimir que tipo de triángulo es o si no existe
+def main():
+    numero = int(input("Número: "))
+    numero2 = int(input("Número: "))
+    numero3 = int(input("Numero: "))
+
+    tipoTriangulo= esTriangulo(numero, numero2, numero3)
+    print(tipoTriangulo)
+
+
+#Llama a la función principal
+main()
