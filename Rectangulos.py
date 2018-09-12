d8ab32a7f3d52a3fb5411d3c2bb888c41d64867f
Alex Leyva <42352939+A01747078@users.noreply.github.com>
2018-09-12 00:58:17 -0500

    Update and rename Rectángulos.py to Rectangulos.py

--- Rectangulos.py
+++ Rectangulos.py
@@ -1,25 +1,30 @@
 # Alex Fernando Leyva Martinez - A01747078 - Grupo: 04
 # Calcula el área y perimetro de dos rectángulos y define cual de ambos es mayor.
 
+
 #Calcula el área del rectángulo 1
 def calcularArea1(largo1, ancho1):
     area1 = largo1 * ancho1
     return area1
+
 
 #Calcula el Perimetro del rectángulo 1
 def calcularPerimetro1(largo1, ancho1):
     perimetro1 = (largo1*2 + ancho1*2)
     return perimetro1
 
+
 #Calcula el área del rectángulo 2
 def calcularArea2(largo2, ancho2):
     area2 = largo2 * ancho2
     return area2
 
+
 #Calcula el Perimetro del rectángulo 2
 def calcularPerimetro2(largo2, ancho2):
     perimetro2 = (largo2*2 + ancho2*2)
     return perimetro2
+
 
 #Calcula que rectángulo tiene el área más grande
 def arearectangulo (area1, area2):
@@ -30,6 +35,7 @@ def arearectangulo (area1, area2):
     else:
         return "Rectangulo 2"
 
+    
 # Lee los datos e imprime las áreas y perimetros además de que evalúa el área de los dos rectángulos e imprime el mayor
 def main():
     largo1 = float(input("Ingresa el largo del rectangulo 1 en m: "))
@@ -47,5 +53,6 @@ def main():
     print("El perimetro del rectángulo 2 es en cm:%.2f" % perimetro2)
     print("El Rectángulo que tiene mayor área es en cm:", area)
 
+    
 #Llama a la función principal
 main()
